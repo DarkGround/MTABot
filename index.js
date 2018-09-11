de5136e7e56389a4805243bb6c1c447b0d1b4142
darkground <blockads31@gmail.com>
2018-09-11 19:35:25 +0300

    Debug cmds v1.2

--- index.js
+++ index.js
@@ -6,22 +6,26 @@ const { Message } = require('discord.js');
 const { User } = require('discord.js');
 const { Presence } = require('discord.js');
 const { Guild } = require('discord.js');
-Bot.login(process.env.token);
+Bot.login('NDg3OTY1ODM2MDQxMjU2OTgw.DnVgmQ.WAg7hAE3aV_yjHGWmBOIO-2Q5NM');
 console.log('MTAshnik v1.0 Launched.')
 console.log('FULL - CONTROL MODE')
 console.log('::help for a help in discord.')
 Bot.on('message',(message)=>{
     if(message.content == "::help"){
         console.log(`[DISCORD] ({${message.guild.name}} ${message.author.username} :: ${message.author.id}) => ${message.content}`)
-        message.channel.send(`Команды: \n ::me <действие> - действие от первого лица \n ::try <действие> - попытать удачу \n ::do <действие> - действие от третьего лица \n ::dot <действие> | <выражение> - сделать что-то сказавши`);
+        var integ = 0;
+        do {      
+            integ = 1
+            message.channel.send(`Команды: \n ::me <действие> - действие от первого лица \n ::try <действие> - попытать удачу \n ::do <действие> - действие от третьего лица \n ::dot <действие> | <выражение> - сделать что-то сказавши`);
+        }while(integ = 0)
     }
 if(message.content.slice(0,5) == '::me ') {
     console.log(`[DISCORD] ({${message.guild.name}} ${message.author.username} :: ${message.author.id}) => ${message.content}`)
     var ttl = message.content.replace('::me ','')
     var integ = 0;
     do {
-    integ = 1
-    message.channel.send(`_${message.author.username}_ **${ttl}**`);
+        integ = 1
+        message.channel.send(`_${message.author.username}_ **${ttl}**`);
     } while(integ = 0)
     return false;
 }
@@ -38,8 +42,8 @@ if(message.content.slice(0,6) == '::try ') {
         luck = 'Удачно'
     }
     do {
-    integ = 1
-    message.channel.send(`_${message.author.username}_ **${ttl}** _(${luck})_`);
+        integ = 1
+        message.channel.send(`_${message.author.username}_ **${ttl}** _(${luck})_`);
     } while(integ = 0)
     return false;
 }
@@ -47,8 +51,10 @@ if(message.content.slice(0,5) == '::do ') {
     console.log(`[DISCORD] ({${message.guild.name}} ${message.author.username} :: ${message.author.id}) => ${message.content}`)
     var ttl = message.content.replace('::do ','')
     var integ = 0;
-    integ = 1
-    message.channel.send(`**${ttl}** ((_${message.author.username}_))`);
+    do {
+        integ = 1
+        message.channel.send(`**${ttl}** ((_${message.author.username}_))`);
+    } while(integ = 0)
     return false;
 }
 if(message.content.slice(0,6) == '::dot ') {
@@ -66,19 +72,37 @@ if(message.content.slice(0,6) == '::dot ') {
         }
     }
     var integ = 0;
-    integ = 1
-    message.channel.send(`_${message.author.username}_ **${ttl2}** _,сказал(а):_ **${ttl}**`);
+    do{
+        integ = 1
+        message.channel.send(`_${message.author.username}_ **${ttl2}** _,сказал(а):_ **${ttl}**`);
+    }while(integ = 0)
     return false;
 }
 if(message.content.slice(0,12) == '::debughelp'){
     if(message.author.id != '297318282724114433'){
         message.channel.send(`Эта команда доступна только для cosmocat.`);
+        return null;
     }
-    console.log(`[DISCORD] ({${message.guild.name}} ${message.author.username} :: ${message.author.id}) => ${message.content}`)
-    message.channel.send(`Команды: \n ::me <действие> - действие от первого лица \n ::try <действие> - попытать удачу \n ::do <действие> - действие от третьего лица \n ::dot <действие> | <выражение> - сделать что-то сказавши`);
+    do{
+        console.log(`[DISCORD] ({${message.guild.name}} ${message.author.username} :: ${message.author.id}) => ${message.content}`)
+        message.channel.send(`Команды отладки: \n ::say <выражение> - сказать от лица бота.`);
+    }while(integ = 0)
+}
+if(message.content.slice(0,6) == '::say ') {
+    if(message.author.id != '297318282724114433'){
+        message.channel.send(`Эта команда доступна только для cosmocat.`);
+        return null;
+    }
+    var ttl = message.content.replace('::say ','')
+    var integ = 0
+    do {
+        integ = 1
+        message.channel.send(`${ttl}`);
+    }while(integ = 0)
+    return false;
 }
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-used = 0;
+integ = 0;
 })
