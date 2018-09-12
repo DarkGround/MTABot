7898e964ec8d721f606ab2b06e7d509685c337ec
darkground <blockads31@gmail.com>
2018-09-12 16:14:36 +0300

    ::dot changes.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { Message } = require('discord.js');
 const { User } = require('discord.js');
 const { Presence } = require('discord.js');
 const { Guild } = require('discord.js');
-Bot.login('NDg3OTY1ODM2MDQxMjU2OTgw.DnVgmQ.WAg7hAE3aV_yjHGWmBOIO-2Q5NM');
+Bot.login(process.env.token);
 console.log('MTAshnik v1.0 Launched.')
 console.log('FULL - CONTROL MODE')
 console.log('::help for a help in discord.')
@@ -43,21 +43,21 @@ if(message.content.slice(0,5) == '::do ') {
         message.channel.send(`**${ttl}** ((_${message.author.username}_))`);
     return false;
 }
-if(message.content.slice(0,6) == '::dot ') {
+if(message.content.slice(0,7) == '::todo ') {
     console.log(`[DISCORD] ({${message.guild.name}} ${message.author.username} :: ${message.author.id}) => ${message.content}`)
-    var ttl = message.content.replace('::dot ','')
-    var ttl2 = message.content.replace('::dot ','')
+    var ttl = message.content.replace('::todo ','')
+    var ttl2 = message.content.replace('::todo ','')
     for (var index = 0; index < ttl2.length; index++) {
-        if(ttl2.charAt(index) == '|'){
+        if(ttl2.charAt(index) == '*'){
             ttl2 = ttl2.replace(ttl2.slice(index - 1,ttl2.length),'')
         }
     }
     for (var index = 0; index < ttl.length; index++) {
-        if(ttl.charAt(index) == '|'){
+        if(ttl.charAt(index) == '*'){
             ttl = ttl.replace(ttl.slice(0,index + 1),'')
         }
     }
-    message.channel.send(`_${message.author.username}_ **${ttl2}** _,сказал(а):_ **${ttl}**`);
+    message.channel.send(`**"${ttl}"**, - сказал(а) _${message.author.username}_, **${ttl2}**`);
     return false;
 }
 if(message.content.slice(0,12) == '::debughelp'){
@@ -91,5 +91,18 @@ if(message.content.slice(0,6) == '::ver') {
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-integ = 0;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///    Roll Thread
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var list = {};
+var found = '';
+if(message.content.replace("::roll")){
+for (var index = 0; index < list.length; index++) {
+    if(list[index].includes(message.author.id) == true){
+        found = list[index]
+        break;
+    }
+}
+console.log(list[index] && ' ' && index)
+}
 })
